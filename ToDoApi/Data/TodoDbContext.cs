3135a5ebc5ea8902f393f6d653d2d23569cf3f11
Baglan <www.baglan888@gmail.com>
2023-08-20 19:25:45 +0600

    first

---
+++ ToDoApi/Data/TodoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class TodoDbContext:DbContext
+    {
+        public TodoDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ ToDoApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
